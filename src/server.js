2b19ea643f1aac06043631a2ff92af10e19b59d0
Mayank Kumar <mynk8076@gmail.com>
2026-09-17 00:24:49 +0530

    first commit

--- src/server.js
+++ src/server.js
@@ -21,6 +21,7 @@ import analyticsRouter from "./services/analytics/routes/analyticsRoutes.js";
  * Initialize Express app
  */
 const app = express();
+app.set('trust proxy', 1);
 
 /**
  * Middlewares
